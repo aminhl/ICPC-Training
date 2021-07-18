b8aa5fc2122b8eac489395763429cfe392394654
aminhl <amin.hlel@esprit.tn>
2021-07-18 10:46:49 +0100

    Sheet#1

---
+++ #1/E.cpp
@@ -0,0 +1,9 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main(){
+    float r;
+    cin >> r;
+    const double pi = 3.141592653;
+    cout << fixed << setprecision(9) << pow(2,r) * pi;
+}
---
+++ #1/G.cpp
@@ -0,0 +1,11 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main(){
+    int x,sum=0;
+    cin >> x;
+    for (int i = 0; i <=x ; ++i) {
+        sum+=i;
+    }
+    cout << sum;
+}
