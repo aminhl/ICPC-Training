e3abec09e00a98fa843e30f942e08a310dcc7fb8
aminhl <amin.hlel@esprit.tn>
2021-07-18 12:15:04 +0100

    Sheet#1

---
+++ #1/N.cpp
@@ -0,0 +1,16 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main(){
+    char x;
+    cin >> x;
+    if (x>=97 && x<=122){
+        x-=32;
+        cout << x;
+    }
+    else if(x>=65 && x<=90)
+    {
+        x+=32;
+        cout << x;
+    }
+}
