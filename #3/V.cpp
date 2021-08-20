b7b3ab92839af8f135f826b4a5a101dcd3569330
aminhl <amin.hlel@esprit.tn>
2021-08-20 09:52:09 +0100

    Sheet#3

---
+++ #3/V.cpp
@@ -0,0 +1,15 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+    int n,m;
+    cin >> n >> m;
+    int arr[n];
+    int freq[100004] = {0};
+    for (int i = 0; i <n ; ++i)
+        cin >> arr[i];
+    for (int i = 0; i <n ; ++i)
+        ++freq[arr[i]];
+    for (int i = 1; i <=m ; ++i)
+        cout << freq[i] << endl;
+}
